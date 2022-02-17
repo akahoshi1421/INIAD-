ac1b4cbaf981ae28a72504f812bc514370ff09cf
unknown <akahoshi.hirokimaimai@gmail.com>
2022-02-18 02:30:57 +0900

    スライド説明ページを追加

--- js/slide_question.js
+++ js/slide_question.js
@@ -1,7 +1,7 @@
 $(function(){
     if(window.location.href == "https://moocs.iniad.org/courses?slideoperate"){
         $(".content-wrapper").empty();
-        const kouji = '<section class="content-header">    <h1>工事中...</h1></section>';
+        const kouji = '<section class="content-header">    <h1>スライド操作説明</h1></section><section class="content container-fluid" id="slideiframediv"><iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQLTyCKAPjmCClgz425A0X_15ms0mkgH1jqWFbhwfp9qIwKmLrlWvTe-Df2n__OZ2qtfo5WTOhk_erN/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe></section>';
         $(".content-wrapper").append(kouji);
     }
 });
