c6d9ed153f36ce2f11b1b32bee640f183a15ccc9
akahoshi1421 <akaho1421@gmail.com>
2022-04-19 22:48:15 +0900

    ファイルドラック機能をとりあえず外した

--- debugs/file_drag.js
+++ debugs/file_drag.js
@@ -21,4 +21,25 @@ $(document).on("drop", ".file-container .file-trigger-btn:enabled", function(e){
     $(this).removeClass("dragover");
     e2 = e.originalEvent;//jQueryがdataTransfer.filesを対応していないため
     $(".file-container .form-control-file")[0].files = e2.dataTransfer.files;
+    $($(".file-container .form-control-file")[0]).change();//プログラムから無理やり値を変えた際、changeイベントが発火しないので強引に発火
 });
+
+/*
+$(document).on("change", ".problem-contents [name][type=file]", function(e){
+    let fd = new FormData();
+    fd.append("file", e.target.files[0]);
+    $.ajax({
+        url: $(".problem-container").data("urlprefix") + "/file/" + $(this).attr("name"),
+        type: "POST",
+        processData: false,
+        contentType: false,
+        dataType: "json",
+        data: fd,
+        context: this,
+        timeout: 30000,
+    }).done(function(data){
+        $(this).val("");
+        console.log("aaaaaaaaaa");
+    })
+});
+*/
