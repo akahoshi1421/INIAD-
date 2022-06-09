66ad4225eca8b41a7a2212db1d2ae7ade46a55cc
akahoshi1421 <akaho1421@gmail.com>
2022-06-09 17:34:20 +0900

    メモ機能のテキストカウント機能がバグっていたのを修正

--- js/moocsmemo_btn.js
+++ js/moocsmemo_btn.js
@@ -8,27 +8,6 @@ function memoui()
         "position": "fixed",
         "z-index": "5"
     });//何故かバグるのでCSSを上書き
-
-    let txtareas = $("textarea");
-    for(let txtarea of txtareas){
-        let txt = $(txtarea).val();
-
-        let txtcnt = txt.length;
-        let wordcnt;
-
-        const spaces = txt.match(/\S+/g);
-        if(spaces){
-            wordcnt = spaces.length;
-        }
-        else{
-            wordcnt = 0;
-        }
-
-        if($(txtarea).next().text().indexOf("文字数:") == -1){
-            const insertcnt = "<p>文字数:" + txtcnt + " 単語数:" + wordcnt + "</p>";
-            $(txtarea).after(insertcnt);
-        }
-    }
 }
 
 $(document).on("click", "#mynote", function(){
