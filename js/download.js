a0d7611f8d84013d76fdd12ffb303b3adf6b7dc1
akahoshi1421 <akaho1421@gmail.com>
2022-07-31 02:08:36 +0900

    試験用Wi-Fiでダウンロードしたスライドを見たときに画像が見れなくなる問題を修正

--- js/download.js
+++ js/download.js
@@ -40,60 +40,133 @@ $(function(){
             const default_html_front = "<div class='row'><div class='col-4'><div class='fix'><h2>目次</h2><hr><ul>";//バーに繋げる
             const default_html_mid = "</ul></div></div><div class='col-8'>"//本体に繋げる
             const default_html_back = "</div>"//最後に繋げる
-            let page_result = "";
-            let page_bar = "";
-            let cnt = 1;
-            for(;;){
-                let title = $(".punch-viewer-svgpage-a11yelement").attr('aria-label');
-                let slide = $('.punch-viewer-svgpage-svgcontainer:last>svg').get(0);
-                let text = ""
 
-                $(slide).find("g").each(function (){
-                    if($(this).attr('aria-label')){
-                        text += ($(this).attr('aria-label') + "<br>")
-                    }
+            let click_cnt = 0;
+            let is_break1 = 0;
+            for(;;){
+                let slide = $('.punch-viewer-svgpage-svgcontainer:last>svg').get(0);
+
+                $(slide).find("image").each(function(i){
+                    let imgURL = $(this).attr("xlink:href");
+
+                    (async () => {
+                        //一旦blobに変換
+                        async function getImage(url){
+                            const res = await fetch(url);
+                            const blob = await res.blob()
+                            return blob;
+                        }
+
+                        const imageData = await getImage(imgURL);
+
+                        let filereader = new FileReader();
+
+                        let changebase64 = $($(slide).find("image")[i]);
+
+                        filereader.onload = function(){
+                            $($(slide).find("image")[i]).attr({
+                                "xlink:href": this.result
+                            });
+                        }
+
+
+
+                        filereader.readAsDataURL(imageData);
+
+                    })()
+                    is_break1++;
+                    //console.log(is_break1);
                 });
-                
-                page_result += "<div class='blank'></div>";
-                page_result += "<div class='row shadow'>";
-                page_result += "<h3 id='" + cnt + "'>" + title + "</h3>";
-                page_result += "<b>" + cnt + "ページ目</b>";
-                page_result += "<div class='col-8'>";
-                page_result += new XMLSerializer().serializeToString(slide);
-                page_result += "</div>";
-                page_result += "<div class='col-4'>";
-                
-                page_result += text;
-                
-                page_result += "</div>";
-                page_result += "</div>";
-                page_result += "<div class='blank'></div>";
-                
-                page_bar += ("<li>" + "<a href='#" + cnt + "'>" + title + "</a></li><div class='blank2'></div>");
 
                 if($(".docs-material-menu-button-flat-default-caption").attr("aria-setsize") == $(".docs-material-menu-button-flat-default-caption").text()){
-                
                     break;
                 }
                 document.dispatchEvent(new KeyboardEvent("keydown",{
                     keyCode: 39
                 }));
-                cnt++;
+                click_cnt++;
             }
+
             
-            const style = "<style>.blank{height: 15px;}.blank2{height: 10px;}ul{height: 700px; width:60%; overflow: auto; overflow-x: auto; list-style: none;}h2{padding-left: 25%;}.fix{position: fixed; width: 50%;}hr{width: 60%;}</style>";
 
-            //const bootstrap = '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">';
+                for(let i = 0; i < click_cnt; i++){
+                    document.dispatchEvent(new KeyboardEvent("keydown",{
+                        keyCode: 37
+                    }));
+                }
 
-            let result = "<!DOCTYPE html><html><head><title>" + name_html + "</title>" + bootstrap + style + "</head>" +
-            "<body>" + default_html_front + page_bar + default_html_mid + page_result + default_html_back + "</body></html>";
+                let front = 0;
 
-            let link = document.createElement("a");
-            link.href = URL.createObjectURL(new Blob([result],{
-                type: "text/html"
-            }));
-            link.download = (name_html + ".html");
-            link.click();
+                let id = setInterval(function(){
+                    //console.log(front);
+                    if(is_break1 == front){
+
+                        let page_result = "";
+                        let page_bar = "";
+                        let cnt = 1;
+                        for(;;){
+                            let title = $(".punch-viewer-svgpage-a11yelement").attr('aria-label');
+                            let slide = $('.punch-viewer-svgpage-svgcontainer:last>svg').get(0);
+            
+                            
+            
+                            let text = ""
+                            
+                            //console.log($($(slide).find("image")[0]).attr("xlink:href"));
+            
+                            $(slide).find("g").each(function (){
+                                if($(this).attr('aria-label')){
+                                    text += ($(this).attr('aria-label') + "<br>")
+                                }
+                            });
+                            
+                            page_result += "<div class='blank'></div>";
+                            page_result += "<div class='row shadow'>";
+                            page_result += "<h3 id='" + cnt + "'>" + title + "</h3>";
+                            page_result += "<b>" + cnt + "ページ目</b>";
+                            page_result += "<div class='col-8'>";
+                            page_result += new XMLSerializer().serializeToString(slide);
+                            page_result += "</div>";
+                            page_result += "<div class='col-4'>";
+                            
+                            page_result += text;
+                            
+                            page_result += "</div>";
+                            page_result += "</div>";
+                            page_result += "<div class='blank'></div>";
+                            
+                            page_bar += ("<li>" + "<a href='#" + cnt + "'>" + title + "</a></li><div class='blank2'></div>");
+            
+                            if($(".docs-material-menu-button-flat-default-caption").attr("aria-setsize") == $(".docs-material-menu-button-flat-default-caption").text()){
+                            
+                                break;
+                            }
+                            document.dispatchEvent(new KeyboardEvent("keydown",{
+                                keyCode: 39
+                            }));
+                            cnt++;
+                        }
+                        
+                        const style = "<style>.blank{height: 15px;}.blank2{height: 10px;}ul{height: 700px; width:60%; overflow: auto; overflow-x: auto; list-style: none;}h2{padding-left: 25%;}.fix{position: fixed; width: 50%;}hr{width: 60%;}</style>";
+            
+                        //const bootstrap = '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">';
+            
+                        let result = "<!DOCTYPE html><html><head><title>" + name_html + "</title>" + bootstrap + style + "</head>" +
+                        "<body>" + default_html_front + page_bar + default_html_mid + page_result + default_html_back + "</body></html>";
+            
+                        let link = document.createElement("a");
+                        link.href = URL.createObjectURL(new Blob([result],{
+                            type: "text/html"
+                        }));
+                        link.download = (name_html + ".html");
+                        link.click();
+
+
+                        clearInterval(id);
+                    }
+                    front = is_break1;
+                }, 500);
+            
         }
     }
 });
